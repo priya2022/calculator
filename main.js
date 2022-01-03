2531b176341d02c9e34b9e786ab761bfae4bee0c
priya2022 <glenraise@gmail.com>
2022-01-03 19:06:59 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+            function ResetForm() 
+            {
+                
+                document.getElementById("FORM1").reset();
+
+            }
+            function change()
+            {
+                var mybody =document.body;
+                mybody.classList.toggle('dark')
+            }
+            function calc(result){
+            form.displayResult.value=form.displayResult.value+result;
+            
+            }
+           
